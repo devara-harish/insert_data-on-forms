c2803f8cf09395d54d84beaae8b6fa71a16b8ad0
devara-harish <devaraharish09@gmail.com>
2023-04-17 11:28:47 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -23,10 +23,48 @@ def insert_webpage(request):
         url=request.POST.get('url')
         email=request.POST['email']
         TO=Topic.objects.get(topic_name=topic)
+
         WO=Webpage.objects.get_or_create(topic_name=TO,name=name,url=url,email=email)[0]
         WO.save()
         return HttpResponse('webpage insertion is done Successfully')
-
-
+        
     return render(request,'insert_webpage.html',d)
+
+
+
+def insert_access(request):
+    LTO=Webpage.objects.all()
+    d={'webpages':LTO}
+
+    if request.method=='POST':
+        name=request.POST['name']
+        author=request.POST['author']
+        date=request.POST['date']
+        WO=Webpage.objects.get(name=name)
+        AO=AccessRecord.objects.get_or_create(name=WO,author=author,date=date)[0]
+        AO.save()
+        return HttpResponse('Webpage insertion is done Successfully')
+
+    return render(request,'insert_access.html',d)
+
+def retrieve_data(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
+    if request.method=='POST':
+        td=request.POST.getlist('topic')
+        print(td)
+        webqueryset=Webpage.objects.none()
+
+        for i in td:
+            webqueryset=webqueryset|Webpage.objects.filter(topic_name=i)
+        d1={'webpages':webqueryset}
+        return render(request,'display_webpage.html',d1)
+    return render(request,'retrieve_data.html',d)
+
+def checkbox(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
+    return render(request,'checkbox.html',d)
+
+
 
